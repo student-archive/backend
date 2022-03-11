8fcf86380ee2cd5cafb9b39f61a5d3d82a4759a8
Daniil <dadyarri@gmail.com>
2022-03-11 13:01:39 +0300

    refactor(initializr): regenerate project

--- backend/src/main/java/ru/giv/star/api/BackendApplication.java
+++ backend/src/main/java/ru/giv/star/api/BackendApplication.java
@@ -1,4 +1,4 @@
-package ru.dadyarri.star.api;
+package ru.giv.star.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(BackendApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
 }
--- backend/src/test/java/ru/giv/star/api/BackendApplicationTests.java
+++ backend/src/test/java/ru/giv/star/api/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.dadyarri.star.api;
+package ru.giv.star.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendApplicationTests {
 
-  @Test
-  void contextLoads() {}
+	@Test
+	void contextLoads() {
+	}
+
 }
