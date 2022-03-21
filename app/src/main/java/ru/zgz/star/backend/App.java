c1a9f61623bf597d51edbf5b12363294f6f73992
Daniil <dadyarri@gmail.com>
2022-03-21 22:43:40 +0300

    feat(src): add spark example

--- app/src/main/java/ru/zgz/star/backend/App.java
+++ app/src/main/java/ru/zgz/star/backend/App.java
@@ -1,11 +1,13 @@
 package ru.zgz.star.backend;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import static spark.Spark.*;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+public class App {
+
+  public static String getGreeting() {
+    return "Hello World";
+  }
+  public static void main(String[] args) {
+    get("/hello", (req, res) -> getGreeting());
+  }
 }
