0d20ca71b76bbacd272e78b6e3543e373d0bb486
Daniil <dadyarri@gmail.com>
2022-03-23 22:09:11 +0300

    fix(models): optimize imports

--- app/src/main/java/ru/zgz/star/backend/models/Speciality.java
+++ app/src/main/java/ru/zgz/star/backend/models/Speciality.java
@@ -1,9 +1,6 @@
 package ru.zgz.star.backend.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
