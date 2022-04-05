b6a4709bb0b05b8c5bc0d4084ad8675320f023c7
Daniil <dadyarri@gmail.com>
2022-04-05 14:05:43 +0300

    feat(docs): add javadocs for Account model

--- app/src/main/java/ru/zgz/star/backend/models/Account.java
+++ app/src/main/java/ru/zgz/star/backend/models/Account.java
@@ -7,6 +7,13 @@ import javax.persistence.Table;
 import java.time.Instant;
 import java.util.UUID;
 
+/**
+ * Model for table <code>account</code>.
+ *
+ * <p>Storing authentication information about user
+ *
+ * @author dadyarri
+ */
 @Entity
 @Table(name = "account")
 public class Account {
@@ -16,36 +23,73 @@ public class Account {
   private String passwordHash;
   private Instant lastLoginDate;
 
+  /**
+   * Sets value for ID
+   *
+   * @param id New ID of user
+   */
   public void setId(UUID id) {
     this.id = id;
   }
 
+  /**
+   * Returns ID
+   *
+   * @return ID of user
+   */
   @Id
   @GeneratedValue
   public UUID getId() {
     return id;
   }
 
+  /**
+   * Sets value for username
+   *
+   * @param username New ID of user
+   */
   public void setUsername(String username) {
     this.username = username;
   }
 
+  /** Returns username
+   *
+   * @return username of user
+   */
   public String getUsername() {
     return username;
   }
 
+  /**
+   * Sets value for hash of password
+   *
+   * @param passwordHash New hash of password
+   */
   public void setPasswordHash(String passwordHash) {
     this.passwordHash = passwordHash;
   }
 
+  /** Returns hash of password
+   *
+   * @return hash of password of user
+   */
   public String getPasswordHash() {
     return passwordHash;
   }
 
+  /**
+   * Updates last date of login
+   *
+   * @param lastLoginDate New login date
+   */
   public void setLastLoginDate(Instant lastLoginDate) {
     this.lastLoginDate = lastLoginDate;
   }
 
+  /** Returns last login date
+   *
+   * @return last login date of user
+   */
   public Instant getLastLoginDate() {
     return lastLoginDate;
   }
