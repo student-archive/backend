5c2a53b1a4ba3280e456107e3d4060a6d5ee505f
Daniil <dadyarri@gmail.com>
2022-04-05 14:12:56 +0300

    feat(docs): add javadocs for App class

--- app/src/main/java/ru/zgz/star/backend/App.java
+++ app/src/main/java/ru/zgz/star/backend/App.java
@@ -2,12 +2,23 @@ package ru.zgz.star.backend;
 
 import static spark.Spark.*;
 
+/**
+ * Entry point of the application
+ */
 public class App {
 
+  /**
+   * Some test method to show how Spark works
+   * @return hello world string
+   */
   public static String getGreeting() {
     return "Hello World";
   }
 
+  /**
+   * Executable method with definition of endpoints
+   * @param args Command-line arguments
+   */
   public static void main(String[] args) {
     get("/hello", (req, res) -> getGreeting());
   }
