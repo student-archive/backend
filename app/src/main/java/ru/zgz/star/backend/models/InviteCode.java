15799a04898e321d35a318dfa0388d62e8ff7693
Daniil <dadyarri@gmail.com>
2022-04-11 21:54:47 +0300

    feat(models): add invite code model

---
+++ app/src/main/java/ru/zgz/star/backend/models/InviteCode.java
@@ -0,0 +1,58 @@
+package ru.zgz.star.backend.models;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "invite_code")
+public class InviteCode {
+  private UUID id;
+
+  @ManyToOne private Account account;
+  private String inviteCode;
+  private boolean isValid;
+  private Instant activationDate;
+
+  @Id
+  @GeneratedValue
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public Account getAccount() {
+    return account;
+  }
+
+  public void setAccount(Account account) {
+    this.account = account;
+  }
+
+  public String getInviteCode() {
+    return inviteCode;
+  }
+
+  public void setInviteCode(String inviteCode) {
+    this.inviteCode = inviteCode;
+  }
+
+  public boolean getIsValid() {
+    return isValid;
+  }
+
+  public void setIsValid(boolean isValid) {
+    this.isValid = isValid;
+  }
+
+  public Instant getActivationDate() {
+    return activationDate;
+  }
+
+  public void setActivationDate(Instant activationDate) {
+    this.activationDate = activationDate;
+  }
+}
