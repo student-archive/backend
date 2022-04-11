b21c9fdcdaa9043dc9b17af950d6b7d3a9a46f38
ironalex <superzhelezov@gmail.com>
2022-04-11 22:52:59 +0300

    feat(models):add model attachmentType

---
+++ app/src/main/java/ru/zgz/star/backend/models/AttachmentType.java
@@ -0,0 +1,32 @@
+package ru.zgz.star.backend.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table (name = "attachment_type")
+public class AttachmentType {
+  private UUID id;
+  private String typeName;
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  @Id
+  @GeneratedValue
+  public UUID getId() {
+    return id;
+  }
+
+  public void setTypeName(String typeName) {
+    this.typeName = typeName;
+  }
+
+  public String getTypeName() {
+    return typeName;
+  }
+}
