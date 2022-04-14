8feb88baec661533840a31250cb0f51ab0f0a3fc
Iogsothot <supercan33vl@gmail.com>
2022-04-14 22:38:21 +0300

    feat(routes) add Page router

---
+++ app/src/main/java/ru/zgz/star/backend/routes/PageRouter.java
@@ -0,0 +1,19 @@
+package ru.zgz.star.backend.routes;
+
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class PageRouter {
+  public static String BASE_URL = "/page/:subjectId";
+
+  public static String getPage(Request request, Response response) {
+    response.type("application/json");
+    return new Gson().toJson(IntStream.range(1, 6).boxed().collect(Collectors.toList()));
+  }
+}
+
+
