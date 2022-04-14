b99cb7060c1d5aef342a3e8b2f2a72cda4a776b9
Daniil <dadyarri@gmail.com>
2022-04-14 21:36:18 +0300

    refactor(routers): rename router

--- app/src/main/java/ru/zgz/star/backend/routes/PostInviteCodeRouter.java
+++ app/src/main/java/ru/zgz/star/backend/routes/PostInviteCodeRouter.java
@@ -2,7 +2,7 @@ package ru.zgz.star.backend.routes;
 
 import com.google.gson.Gson;
 
-public class InviteCodeRouter {
+public class PostInviteCodeRouter {
     public static final String BASE_URL = "/inviteCode";
 
     public String postInviteCode() {
