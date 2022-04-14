ebd8a17e11a18d9ffa25e7fa8d58af113a5ae064
Iogsothot <supercan33vl@gmail.com>
2022-04-14 22:47:06 +0300

    feat(routes) add  attachment router

---
+++ app/src/main/java/ru/zgz/star/backend/routes/AttachmentRouter.java
@@ -0,0 +1,17 @@
+package ru.zgz.star.backend.routes;
+
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class AttachmentRouter {
+  public static String BASE_URL = "/attachment/:subjectId";
+
+  public static String getAttachment(Request request, Response response) {
+    response.type("application/json");
+    return new Gson().toJson(IntStream.range(1, 6).boxed().collect(Collectors.toList()));
+  }
+}
