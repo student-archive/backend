032c0ec373ba52222d760a35f18576ded162b469
Daniil <dadyarri@gmail.com>
2022-04-14 21:49:19 +0300

    feat(routers): add base router

---
+++ app/src/main/java/ru/zgz/star/backend/routes/BaseRouter.java
@@ -0,0 +1,29 @@
+package ru.zgz.star.backend.routes;
+
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+
+public abstract class BaseRouter {
+  public static String BASE_URL = "";
+
+  public static String get(Request request, Response response) {
+    response.status(405);
+    return new Gson().toJson(new Error("Method not allowed"));
+  }
+
+  public static String post(Request request, Response response) {
+    response.status(405);
+    return new Gson().toJson(new Error("Method not allowed"));
+  }
+
+  public static String put(Request request, Response response) {
+    response.status(405);
+    return new Gson().toJson(new Error("Method not allowed"));
+  }
+
+  public static String delete(Request request, Response response) {
+    response.status(405);
+    return new Gson().toJson(new Error("Method not allowed"));
+  }
+}
