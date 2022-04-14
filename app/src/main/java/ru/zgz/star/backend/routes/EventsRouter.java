1efc83f856a09209a2e806274f36e686cdeba33d
ironalex <superzhelezov@gmail.com>
2022-04-14 20:52:10 +0300

    feat(routers): add EventsRouter

---
+++ app/src/main/java/ru/zgz/star/backend/routes/EventsRouter.java
@@ -0,0 +1,17 @@
+package ru.zgz.star.backend.routes;
+
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class EventsRouter {
+  public static String BASE_URL = "/events/:id";
+
+  public static String getEvents(Request request, Response response) {
+    response.type("application/json");
+    return new Gson().toJson("id:" + request.params(":id"));
+  }
+}
