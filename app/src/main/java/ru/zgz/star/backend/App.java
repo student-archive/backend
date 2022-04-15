e7b47a6b460d75a311841decd1191ccf1fa91293
Daniil <dadyarri@gmail.com>
2022-04-15 16:33:48 +0300

    feat(src): add starting spring application

--- app/src/main/java/ru/zgz/star/backend/App.java
+++ app/src/main/java/ru/zgz/star/backend/App.java
@@ -1,11 +1,27 @@
 package ru.zgz.star.backend;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.zgz.star.backend.repositories.AccountRepository;
+
 /** Entry point of the application */
+@SpringBootApplication
 public class App {
   /**
    * Executable method with definition of endpoints and connecting to database
    *
    * @param args Command-line arguments
    */
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner demo(AccountRepository accountRepository) {
+    return (args) -> {
+      accountRepository.findAll().forEach(System.out::println);
+    };
+  }
 }
