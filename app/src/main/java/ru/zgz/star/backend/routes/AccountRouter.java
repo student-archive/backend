e6843b945a85e71c7d31d5d7b04f8e3bfc04c3a4
Daniil <dadyarri@gmail.com>
2022-04-20 00:37:54 +0300

    feat(endpoints): add some boilerplate code to endpoint (why it isn't working?)

--- app/src/main/java/ru/zgz/star/backend/routes/AccountRouter.java
+++ app/src/main/java/ru/zgz/star/backend/routes/AccountRouter.java
@@ -1,8 +1,15 @@
 package ru.zgz.star.backend.routes;
 
 import com.google.gson.Gson;
+import jakarta.persistence.EntityManager;
+import ru.zgz.star.backend.models.Account;
+import ru.zgz.star.backend.util.HibernateUtil;
 import spark.Request;
 import spark.Response;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
 
 public class AccountRouter {
 
@@ -10,7 +17,19 @@ public class AccountRouter {
 
   public static String getExactAccount(Request request, Response response) {
     response.type("application/json");
-    return new Gson().toJson("id:" + request.params(":id"));
+    var sessionFactory = HibernateUtil.getSessionFactory();
+    EntityManager em = sessionFactory.createEntityManager();
+    em.getTransaction().begin();
+    Account entity = new Account(UUID.randomUUID(), "test", "someSuperSecurePassword",
+      Instant.now());
+    em.persist(entity);  // FIXME: detached entity passed to persist
+    List<Account> result =
+        em.createQuery("select a from Account a where id=:id", Account.class)
+            .setParameter("id", UUID.fromString(request.params("id")))
+            .getResultList();  // FIXME: Unable to make field private final long java.time.Instant.seconds accessible
+    em.getTransaction().commit();
+    em.close();
+    return new Gson().toJson(result);
   }
 
   public static String deleteExactAccount(Request request, Response response) {
@@ -22,5 +41,4 @@ public class AccountRouter {
     response.type("application/json");
     return new Gson().toJson("OK");
   }
-
 }
