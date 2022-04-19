fc450413f6b873ba983a909b21cbf55d9d90a99c
Daniil <dadyarri@gmail.com>
2022-04-20 00:34:01 +0300

    refactor(src): update main class

--- app/src/main/java/ru/zgz/star/backend/App.java
+++ app/src/main/java/ru/zgz/star/backend/App.java
@@ -1,7 +1,5 @@
 package ru.zgz.star.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.hibernate.cfg.Configuration;
 import ru.zgz.star.backend.routes.AccountRouter;
 import ru.zgz.star.backend.routes.EventPrioritiesRouter;
 import ru.zgz.star.backend.routes.GetSoftwareRouter;
@@ -12,24 +10,11 @@ import static spark.Spark.get;
 public class App {
 
   /**
-   * Some test method to show how Spark works
-   *
-   * @return hello world string
-   */
-  public static String getGreeting() {
-    return "Hello World";
-  }
-
-  /**
-   * Executable method with definition of endpoints and connecting to database
+   * Executable method with definition of endpoints
    *
    * @param args Command-line arguments
    */
   public static void main(String[] args) {
-    Configuration cfg = new Configuration();
-    Dotenv dotenv = Dotenv.load();
-    cfg.setProperty("hibernate.connection.url", dotenv.get("DATABASE_URL"));
-
     get(AccountRouter.BASE_URL, AccountRouter::getExactAccount);
     get(EventPrioritiesRouter.BASE_URL, EventPrioritiesRouter::getPriorities);
     get(GetSoftwareRouter.BASE_URL, GetSoftwareRouter::getListSoftware);
