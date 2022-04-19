4a7f94fddd406113aeee5342ad9c4fd372047974
Daniil <dadyarri@gmail.com>
2022-04-20 00:33:18 +0300

    feat(db): add util to get hibernate session factory

---
+++ app/src/main/java/ru/zgz/star/backend/util/HibernateUtil.java
@@ -0,0 +1,30 @@
+package ru.zgz.star.backend.util;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.zgz.star.backend.models.Account;
+
+public class HibernateUtil {
+  private static final SessionFactory sessionFactory;
+
+  static {
+    try {
+      var cfg = new Configuration();
+      Dotenv dotenv = Dotenv.load();
+      cfg.setProperty("hibernate.connection.url", dotenv.get("DATABASE_URL"));
+      cfg.setProperty("hibernate.connection.username", dotenv.get("DATABASE_USER"));
+      cfg.setProperty("hibernate.connection.password", dotenv.get("DATABASE_PASSWORD"));
+      cfg.addAnnotatedClass(Account.class);
+
+      sessionFactory = cfg.buildSessionFactory();
+    } catch (Throwable ex) {
+      System.err.println("Initial SessionFactory creation failed." + ex);
+      throw new ExceptionInInitializerError(ex);
+    }
+  }
+
+  public static SessionFactory getSessionFactory() {
+    return sessionFactory;
+  }
+}
