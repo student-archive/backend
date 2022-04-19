ac8c399a010e89d018dd34d6171e419c35a03469
Daniil <dadyarri@gmail.com>
2022-04-19 21:45:07 +0300

    fix(models): update repository

--- app/src/main/java/ru/zgz/star/backend/repositories/AccountRepository.java
+++ app/src/main/java/ru/zgz/star/backend/repositories/AccountRepository.java
@@ -1,10 +1,15 @@
 package ru.zgz.star.backend.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.zgz.star.backend.models.Account;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface AccountRepository extends CrudRepository<Account, UUID> {
-
+@RepositoryRestResource(path = "accounts")
+public interface AccountRepository extends PagingAndSortingRepository<Account, UUID> {
+  List<Account> findByUsername(@Param("name") String username);
 }
