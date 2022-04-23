ece66591e2aeb73eacd90b048b6fe2352ec7a733
Daniil <dadyarri@gmail.com>
2022-04-23 11:17:34 +0300

    feat(src): add openAPI specs

--- app/src/main/java/ru/zgz/star/backend/App.java
+++ app/src/main/java/ru/zgz/star/backend/App.java
@@ -1,6 +1,26 @@
 package ru.zgz.star.backend;
 
-/** Entry point of the application */
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Star Backend",
+            version = "0.0.1",
+            contact = @Contact(name = "ZGZ Team", email = "dadyarri@gmail.com"),
+            license =
+                @License(
+                    name = "MIT",
+                    url = "https://github.com/student-archive/backend/blob/master/LICENSE.txt")),
+    tags = {
+      @Tag(name = "persons", description = "Operations about persons"),
+      @Tag(name = "quizzes", description = "Operations about quizzes"),
+      @Tag(name = "content", description = "Operations about content"),
+    })
 public class App {
 
   /**
@@ -9,6 +29,5 @@ public class App {
    * @param args Command-line arguments
    */
   public static void main(String[] args) {
-
   }
 }
