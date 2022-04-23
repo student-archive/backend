aae877bea3a235a7faf5ea9a260420b3ee4f2242
Daniil <dadyarri@gmail.com>
2022-04-23 12:06:58 +0300

    feat(utils): add util to find classes in package

---
+++ app/src/main/java/ru/zgz/star/backend/util/ClassUtil.java
@@ -0,0 +1,19 @@
+package ru.zgz.star.backend.util;
+
+import com.google.common.reflect.ClassPath;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClassUtil {
+  public static Set<Class<?>> findAllClasses(String packageName) throws IOException {
+    return ClassPath.from(ClassLoader.getSystemClassLoader())
+      .getAllClasses()
+      .stream()
+      .filter(clazz -> clazz.getPackageName()
+        .equalsIgnoreCase(packageName))
+      .map(clazz -> clazz.load())
+      .collect(Collectors.toSet());
+  }
+}
