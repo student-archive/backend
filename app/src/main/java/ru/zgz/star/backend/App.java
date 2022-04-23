411526478d969195157bcb6aac3673b49038c824
Daniil <dadyarri@gmail.com>
2022-04-23 09:53:20 +0300

    fix(src): remove old init

--- app/src/main/java/ru/zgz/star/backend/App.java
+++ app/src/main/java/ru/zgz/star/backend/App.java
@@ -1,11 +1,5 @@
 package ru.zgz.star.backend;
 
-import ru.zgz.star.backend.routes.AccountRouter;
-import ru.zgz.star.backend.routes.EventPrioritiesRouter;
-import ru.zgz.star.backend.routes.GetSoftwareRouter;
-
-import static spark.Spark.get;
-
 /** Entry point of the application */
 public class App {
 
@@ -15,8 +9,6 @@ public class App {
    * @param args Command-line arguments
    */
   public static void main(String[] args) {
-    get(AccountRouter.BASE_URL, AccountRouter::getExactAccount);
-    get(EventPrioritiesRouter.BASE_URL, EventPrioritiesRouter::getPriorities);
-    get(GetSoftwareRouter.BASE_URL, GetSoftwareRouter::getListSoftware);
+
   }
 }
