478addd8a1a20977feae32eedf67ff63ed97131f
Daniil <dadyarri@gmail.com>
2022-04-24 22:28:53 +0300

    fix(database): fix findAll in DAO

--- app/src/main/java/ru/zgz/star/backend/repository/DAO.java
+++ app/src/main/java/ru/zgz/star/backend/repository/DAO.java
@@ -2,14 +2,23 @@ package ru.zgz.star.backend.repository;
 
 import com.google.common.base.CaseFormat;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.zgz.star.backend.routes.AccountRouter;
 import ru.zgz.star.backend.util.HibernateUtil;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 import java.util.UUID;
 
 public class DAO<T> {
 
+  public static Logger logger = LoggerFactory.getLogger(AccountRouter.class);
   private final Class<T> type;
 
   public DAO(Class<T> tClass) {
@@ -29,9 +38,14 @@ public class DAO<T> {
   public List<T> findAll() {
     EntityManager em = getEntityManager();
     em.getTransaction().begin();
-    String table_name = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, "SomeInput");
-    return em.createQuery(String.format("select %s from %s", table_name, table_name), type)
-        .getResultList();
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<T> cq = cb.createQuery(type);
+    Root<T> root = cq.from(type);
+    cq.select(root);
+    List<T> result = em.createQuery(cq).getResultList();
+    closeConnection(em);
+    return result;
+
   }
 
   public T findById(UUID id) {
