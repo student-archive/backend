9caf19e3fdc3b75369f1d4e0442a002787799ea3
Daniil <dadyarri@gmail.com>
2022-04-25 12:55:22 +0300

    feat(docs): add javadocs for ClassUtil

--- app/src/main/java/ru/zgz/star/backend/util/ClassUtil.java
+++ app/src/main/java/ru/zgz/star/backend/util/ClassUtil.java
@@ -6,7 +6,15 @@ import java.io.IOException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/** Set of tools for classes */
 public class ClassUtil {
+  /**
+   * Finds all classes in a package
+   *
+   * @param packageName name of package
+   * @return Set of classes
+   * @throws IOException if package was not found
+   */
   public static Set<Class<?>> findAllClasses(String packageName) throws IOException {
     return ClassPath.from(ClassLoader.getSystemClassLoader()).getAllClasses().stream()
         .filter(clazz -> clazz.getPackageName().equalsIgnoreCase(packageName))
