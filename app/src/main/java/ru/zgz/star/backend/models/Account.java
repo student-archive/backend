15fa55ee99ded8bbbea3bf67db596593eb69410a
ironalex <superzhelezov@gmail.com>
2022-04-30 21:08:58 +0300

    style(models): get rid of wild card imports

--- app/src/main/java/ru/zgz/star/backend/models/Account.java
+++ app/src/main/java/ru/zgz/star/backend/models/Account.java
@@ -1,6 +1,10 @@
 package ru.zgz.star.backend.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.time.Instant;
