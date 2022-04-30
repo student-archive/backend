06051499cadb3d58cc7680088770476c8ba27d83
Iogsothot <supercan33vl@gmail.com>
2022-04-30 20:46:44 +0300

    refactor(model)change model Tutor

--- app/src/main/java/ru/zgz/star/backend/models/Tutor.java
+++ app/src/main/java/ru/zgz/star/backend/models/Tutor.java
@@ -1,9 +1,8 @@
 package ru.zgz.star.backend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
 import java.util.UUID;
 
 /**
@@ -16,78 +15,114 @@ import java.util.UUID;
 @Entity
 @Table(name = "tutor")
 public class Tutor {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(name = "id")
   private UUID id;
+
+  @Column(name = "first_name")
   private String firstName;
+
+  @Column(name = "last_name")
   private String lastName;
+
+  @Column(name = "patronymic")
   private String patronymic;
+
+  @Column(name = "email")
   private String email;
+
+  @Column(name = "phone")
   private String phone;
+
+  @Column(name = "link")
   private String link;
+
+  @Column(name = "is_working")
   private boolean isWorking;
 
-  @Id
-  @GeneratedValue
+  public Tutor() {}
+
+  public Tutor(String firstName,String lastName,String patronymic,String email,String phone,String link,Boolean isWorking) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.patronymic = patronymic;
+    this.email = email;
+    this.phone = link;
+    this.isWorking = isWorking;
+
+  }
+
   public UUID getId() {
     return id;
   }
 
-  public void setId(UUID id) {
+  public Tutor setId(UUID id) {
     this.id = id;
+    return this;
   }
 
   public String getFirstName() {
     return firstName;
   }
 
-  public void setFirstName(String firstName) {
+  public Tutor setFirstName(String firstName) {
     this.firstName = firstName;
+    return this;
   }
 
   public String getLastName() {
     return lastName;
   }
 
-  public void setLastName(String lastName) {
+  public Tutor setLastName(String lastName) {
     this.lastName = lastName;
+    return this;
   }
 
   public String getPatronymic() {
     return patronymic;
   }
 
-  public void setPatronymic(String patronymic) {
+  public Tutor setPatronymic(String patronymic) {
     this.patronymic = patronymic;
+    return this;
   }
 
   public String getEmail() {
     return email;
   }
 
-  public void setEmail(String email) {
+  public Tutor setEmail(String email) {
     this.email = email;
+    return this;
   }
 
   public String getPhone() {
     return phone;
   }
 
-  public void setPhone(String phone) {
+  public Tutor setPhone(String phone) {
     this.phone = phone;
+    return this;
   }
 
   public String getLink() {
     return link;
   }
 
-  public void setLink(String link) {
+  public Tutor setLink(String link) {
     this.link = link;
+    return this;
   }
 
   public boolean isIsWorking() {
     return isWorking;
   }
 
-  public void setIsWorking(boolean isWorking) {
+  public Tutor setIsWorking(boolean isWorking) {
     this.isWorking = isWorking;
+    return this;
   }
 }
