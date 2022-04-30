00e7ef21763af23d4ffac1725fec9783a906bc31
Iogsothot <supercan33vl@gmail.com>
2022-04-30 18:58:06 +0300

    refactor(model)change model QuizResult

--- app/src/main/java/ru/zgz/star/backend/models/QuizResult.java
+++ app/src/main/java/ru/zgz/star/backend/models/QuizResult.java
@@ -1,10 +1,7 @@
 package ru.zgz.star.backend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -19,15 +16,32 @@ import java.util.UUID;
 @Entity
 @Table(name = "quiz_result")
 public class QuizResult {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(name = "id")
   private UUID id;
+
+  @Column(name = "result")
   private Integer result;
+
+  @Column(name = "quiz_submit_date")
   private Instant quizSubmitDate;
 
   @ManyToOne private Quiz quiz;
   @ManyToOne private User user;
 
-  public void setId(UUID id) {
+  public QuizResult() {}
+
+  public QuizResult(
+    Integer result,Instant quizSubmitDate) {
+    this.result = result;
+    this.quizSubmitDate = quizSubmitDate;
+  }
+
+  public QuizResult setId(UUID id) {
     this.id = id;
+    return this;
   }
 
   @Id
@@ -36,32 +50,36 @@ public class QuizResult {
     return id;
   }
 
-  public void setResult(Integer result) {
+  public QuizResult setResult(Integer result) {
     this.result = result;
+    return this;
   }
 
   public Integer getResult() {
     return result;
   }
 
-  public void setQuizSubmitDate(Instant quizSubmitDate) {
+  public QuizResult setQuizSubmitDate(Instant quizSubmitDate) {
     this.quizSubmitDate = quizSubmitDate;
+    return this;
   }
 
   public Instant getQuizSubmitDate() {
     return quizSubmitDate;
   }
 
-  public void setQuiz(Quiz quiz) {
+  public QuizResult setQuiz(Quiz quiz) {
     this.quiz = quiz;
+    return this;
   }
 
   public Quiz getQuiz() {
     return quiz;
   }
 
-  public void setUser(User user) {
+  public QuizResult setUser(User user) {
     this.user = user;
+    return this;
   }
 
   public User getUser() {
