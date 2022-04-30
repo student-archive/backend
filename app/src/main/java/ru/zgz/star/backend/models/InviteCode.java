4040c7646e7f679e17dc3d8189f05acdd7702b24
ironalex <superzhelezov@gmail.com>
2022-04-30 20:24:01 +0300

    refactor(models): change InviteCode

--- app/src/main/java/ru/zgz/star/backend/models/InviteCode.java
+++ app/src/main/java/ru/zgz/star/backend/models/InviteCode.java
@@ -1,10 +1,12 @@
 package ru.zgz.star.backend.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -19,52 +21,74 @@ import java.util.UUID;
 @Entity
 @Table(name = "invite_code")
 public class InviteCode {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(name = "id")
   private UUID id;
 
-  @ManyToOne private Account account;
+  @Column(name = "invite_code")
   private String inviteCode;
+
+  @Column(name = "is_valid")
   private boolean isValid;
+
+  @Column(name = "activated_date")
   private Instant activationDate;
 
-  @Id
-  @GeneratedValue
+  @ManyToOne private Account account;
+
+  public InviteCode() {}
+
+  public InviteCode(String inviteCode, boolean isValid, Instant activationDate, Account account) {
+    this.inviteCode = inviteCode;
+    this.isValid = isValid;
+    this.activationDate = activationDate;
+    this.account = account;
+  }
+
   public UUID getId() {
     return id;
   }
 
-  public void setId(UUID id) {
+  public InviteCode setId(UUID id) {
     this.id = id;
+    return this;
   }
 
   public Account getAccount() {
     return account;
   }
 
-  public void setAccount(Account account) {
+  public InviteCode setAccount(Account account) {
     this.account = account;
+    return this;
   }
 
   public String getInviteCode() {
     return inviteCode;
   }
 
-  public void setInviteCode(String inviteCode) {
+  public InviteCode setInviteCode(String inviteCode) {
     this.inviteCode = inviteCode;
+    return this;
   }
 
   public boolean getIsValid() {
     return isValid;
   }
 
-  public void setIsValid(boolean isValid) {
+  public InviteCode setIsValid(boolean isValid) {
     this.isValid = isValid;
+    return this;
   }
 
   public Instant getActivationDate() {
     return activationDate;
   }
 
-  public void setActivationDate(Instant activationDate) {
+  public InviteCode setActivationDate(Instant activationDate) {
     this.activationDate = activationDate;
+    return this;
   }
 }
