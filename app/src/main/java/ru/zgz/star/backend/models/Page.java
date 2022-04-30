f08de052ca01a6dcda5ec0922a28a48a83669869
ironalex <superzhelezov@gmail.com>
2022-04-30 20:39:16 +0300

    refactor(models): change Page

--- app/src/main/java/ru/zgz/star/backend/models/Page.java
+++ app/src/main/java/ru/zgz/star/backend/models/Page.java
@@ -1,11 +1,13 @@
 package ru.zgz.star.backend.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.Set;
 import java.util.UUID;
@@ -20,40 +22,56 @@ import java.util.UUID;
 @Entity
 @Table(name = "page")
 public class Page {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(name = "id")
   private UUID id;
+
+  @Column(name = "link")
   private String link;
 
   @ManyToOne private Subject subject;
   @ManyToMany private Set<Attachment> attachment;
 
-  public void setId(UUID id) {
-    this.id = id;
+  public Page() {}
+
+  public Page(String link, Subject subject, Set<Attachment> attachment) {
+    this.link = link;
+    this.subject = subject;
+    this.attachment = attachment;
   }
 
-  @Id
-  @GeneratedValue
+  public Page setId(UUID id) {
+    this.id = id;
+    return this;
+  }
+
   public UUID getId() {
     return id;
   }
 
-  public void setLink(String link) {
+  public Page setLink(String link) {
     this.link = link;
+    return this;
   }
 
   public String getLink() {
     return link;
   }
 
-  public void setSubject(Subject subject) {
+  public Page setSubject(Subject subject) {
     this.subject = subject;
+    return this;
   }
 
   public Subject getSubject() {
     return subject;
   }
 
-  public void setAttachment(Set<Attachment> attachment) {
+  public Page setAttachment(Set<Attachment> attachment) {
     this.attachment = attachment;
+    return this;
   }
 
   public Set<Attachment> getAttachment() {
