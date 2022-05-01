6a68d1e712530f04750a37c5b17fe2555e3e7ffc
Daniil <dadyarri@gmail.com>
2022-05-01 13:56:46 +0300

    refactor(test): add test spec's description; reformat test's title

--- app/src/test/groovy/DatabaseConnectionSpecification.groovy
+++ app/src/test/groovy/DatabaseConnectionSpecification.groovy
@@ -1,8 +1,10 @@
 import ru.zgz.star.backend.util.HibernateUtil
 import spock.lang.Specification
+import spock.lang.Title
 
-class DatabaseConnectionSpecification extends Specification{
-    def "should create HibernateUtil without exceptions" () {
+@Title("Tests connection to database and DAO")
+class DatabaseConnectionSpecification extends Specification {
+    def "Should create HibernateUtil without exceptions"() {
         when:
         new HibernateUtil()
         then:
