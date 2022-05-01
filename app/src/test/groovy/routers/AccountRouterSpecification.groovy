57ce54b81b02b177d0392102ef524dca8bab37c7
Daniil <dadyarri@gmail.com>
2022-05-01 15:20:49 +0300

    feat(test): split one giant test to several small

--- app/src/test/groovy/routers/AccountRouterSpecification.groovy
+++ app/src/test/groovy/routers/AccountRouterSpecification.groovy
@@ -10,10 +10,11 @@ import spock.lang.Specification
 class AccountRouterSpecification extends Specification {
     @Shared
             BASE_URL = "http://localhost:4567/account"
+    @Shared
+            id = "ebbce63a-ece0-4aec-9b95-0eefb63113e6"
 
-    def "GET request should return exactly one account"() {
+    def "GET request should return valid json"() {
         given:
-        def id = "ebbce63a-ece0-4aec-9b95-0eefb63113e6"
         def response = Unirest.get("${BASE_URL}/${id}").asJson()
 
         when:
@@ -21,8 +22,32 @@ class AccountRouterSpecification extends Specification {
 
         then:
         response.getStatus() == 200
+        account != null
         notThrown(JsonParseException)
+    }
+
+    def "GET request should return exactly one account" () {
+        given:
+        def response = Unirest.get("${BASE_URL}/${id}").asJson()
+
+        when:
+        JsonParser.parseString(response.getBody().toString()).getAsJsonObject()
+
+        then:
+        response.getStatus() == 200
         notThrown(IllegalStateException)
+    }
+
+
+    def "GET request should return account with requested id"() {
+        given:
+        def response = Unirest.get("${BASE_URL}/${id}").asJson()
+
+        when:
+        def account = JsonParser.parseString(response.getBody().toString()).getAsJsonObject()
+
+        then:
+        response.getStatus() == 200
         account != null
         account.get("id").getAsString() == id.toString()
     }
