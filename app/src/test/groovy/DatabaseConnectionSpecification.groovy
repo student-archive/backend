d6d71680e37960aa78bdceb4ff3865c87d6c92df
Daniil <dadyarri@gmail.com>
2022-05-01 13:57:09 +0300

    feat(test): add test for select with DAO

--- app/src/test/groovy/DatabaseConnectionSpecification.groovy
+++ app/src/test/groovy/DatabaseConnectionSpecification.groovy
@@ -1,3 +1,5 @@
+import ru.zgz.star.backend.models.Account
+import ru.zgz.star.backend.repository.DAO
 import ru.zgz.star.backend.util.HibernateUtil
 import spock.lang.Specification
 import spock.lang.Title
@@ -9,5 +11,16 @@ class DatabaseConnectionSpecification extends Specification {
         new HibernateUtil()
         then:
         notThrown(ExceptionInInitializerError)
+    }
+
+    def "Select from accounts table should return 14 rows"() {
+        given:
+        DAO<Account> dao = new DAO<>(Account.class)
+
+        when:
+        List<Account> accounts = dao.findAll()
+
+        then:
+        accounts.size() == 14
     }
 }
