ee57ffd0ee47e86125d46088f54c859228737383
Daniil <dadyarri@gmail.com>
2022-05-02 20:18:45 +0300

    fix(models): disable all models

--- app/src/main/java/ru/zgz/star/backend/util/HibernateUtil.java
+++ app/src/main/java/ru/zgz/star/backend/util/HibernateUtil.java
@@ -2,6 +2,7 @@ package ru.zgz.star.backend.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import ru.zgz.star.backend.models.Account;
 
 /** Set of utils to work with hibernate */
 public class HibernateUtil {
@@ -18,7 +19,10 @@ public class HibernateUtil {
         cfg.setProperty("hibernate.connection.url", System.getenv("TEST_DATABASE_URL"));
       }
 
-      ClassUtil.findAllClasses("ru.zgz.star.backend.models").forEach(cfg::addAnnotatedClass);
+      cfg.addAnnotatedClass(Account.class);
+
+      // TODO: Add this instead of above, when (relations between) models will be ready
+      //  ClassUtil.findAllClasses("ru.zgz.star.backend.models").forEach(cfg::addAnnotatedClass);
 
       sessionFactory = cfg.buildSessionFactory();
     } catch (Throwable ex) {
