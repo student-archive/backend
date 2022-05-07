e40b2a1ff4367ac6e6b329c0d3b15e0f5324af3f
Daniil <dadyarri@gmail.com>
2022-05-07 21:04:08 +0300

    fix(util): add choosing database url depends on environment

--- src/main/java/ru/zgz/star/backend/util/DbUtil.java
+++ src/main/java/ru/zgz/star/backend/util/DbUtil.java
@@ -11,10 +11,16 @@ public class DbUtil {
     if (connection == null) {
       try {
         String driver = "org.postgresql.Driver";
-        String url = System.getenv("DATABASE_URL");
-        String user = System.getenv("DATABASE_USERNAME");
+        String url;
+        if (System.getenv("ENV").equals("DEV")) {
+          url = System.getenv("TEST_DATABASE_URL");
+        } else {
+          url = System.getenv("DATABASE_URL");
+        }
+        String user = System.getenv("DATABASE_USER");
         String password = System.getenv("DATABASE_PASSWORD");
         Class.forName(driver);
+
         connection = DriverManager.getConnection(url, user, password);
         connection.setAutoCommit(false);
       } catch (ClassNotFoundException | SQLException e) {
