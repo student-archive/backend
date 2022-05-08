afea58fa8836f419b6a52897c5285690c82635e5
Daniil <dadyarri@gmail.com>
2022-05-08 21:16:56 +0300

    feat(test): adopt account router's tests to jdbc

--- src/test/groovy/routers/AccountRouterSpecification.groovy
+++ src/test/groovy/routers/AccountRouterSpecification.groovy
@@ -1,53 +1,68 @@
 package routers
 
+import com.google.gson.Gson
 import com.google.gson.JsonParseException
-import com.google.gson.JsonParser
 import com.mashape.unirest.http.Unirest
-import spock.lang.Ignore
+import ru.zgz.star.backend.daos.AccountDao
+import ru.zgz.star.backend.models.Account
 import spock.lang.Shared
 
-@Ignore
 class AccountRouterSpecification extends BaseRouterSpecification {
-    @Shared
-            BASE_URL = "http://localhost:4567/account"
-    @Shared
-            id = "ebbce63a-ece0-4aec-9b95-0eefb63113e6"
+  @Shared
+          BASE_URL = "http://localhost:4567/account"
 
-    def "GET request should return valid json"() {
-        given:
-        def response = Unirest.get("${BASE_URL}/${id}").asJson()
+  @Shared
+          sampleAccount = null
 
-        when:
-        def account = JsonParser.parseString(response.getBody().toString()).getAsJsonObject()
+  def setup() {
+    def dao = new AccountDao()
+    dao.add(new Account()
+            .setEmail("test@example.com")
+            .setPasswordHash("superDuperSecurePassword")
+            .setLastActiveDate(1234567))
+    sampleAccount = dao.getByEmail("test@example.com")
+  }
 
-        then:
-        response.getStatus() == 200
-        account != null
-        notThrown(JsonParseException)
-    }
+  def "GET request should return valid json"() {
+    given:
+      def response = Unirest.get("${BASE_URL}/${this.sampleAccount.id.toString()}").asString()
 
-    def "GET request should return exactly one account"() {
-        given:
-        def response = Unirest.get("${BASE_URL}/${id}").asJson()
+    when:
+      def account = new Gson().fromJson(response.body, Account)
 
-        when:
-        JsonParser.parseString(response.getBody().toString()).getAsJsonObject()
+    then:
+      response.getStatus() == 200
+      account != null
+      notThrown(JsonParseException)
+  }
 
-        then:
-        response.getStatus() == 200
-        notThrown(IllegalStateException)
-    }
+  def "GET request should return exactly one account"() {
+    given:
+      def response = Unirest.get("${BASE_URL}/${this.sampleAccount.id.toString()}").asString()
 
-    def "GET request should return account with requested id"() {
-        given:
-        def response = Unirest.get("${BASE_URL}/${id}").asJson()
+    when:
+      new Gson().fromJson(response.body, Account)
 
-        when:
-        def account = JsonParser.parseString(response.getBody().toString()).getAsJsonObject()
+    then:
+      response.getStatus() == 200
+      notThrown(IllegalStateException)
+  }
 
-        then:
-        response.getStatus() == 200
-        account != null
-        account.get("id").getAsString() == id.toString()
-    }
+  def "GET request should return account with requested id"() {
+    given:
+      def response = Unirest.get("${BASE_URL}/${this.sampleAccount.id.toString()}").asString()
+
+    when:
+      def account = new Gson().fromJson(response.body, Account)
+
+    then:
+      response.getStatus() == 200
+      account != null
+      account.id == sampleAccount.id
+  }
+
+  def cleanup() {
+    def dao = new AccountDao()
+    dao.deleteAll()
+  }
 }
