72d52037dc36e6de4e4b913b1572f3411250fc72
Daniil <dadyarri@gmail.com>
2022-05-09 11:02:12 +0300

    feat(docs): add javadocs

--- src/main/java/ru/zgz/star/backend/responses/DeletedResponse.java
+++ src/main/java/ru/zgz/star/backend/responses/DeletedResponse.java
@@ -2,6 +2,9 @@ package ru.zgz.star.backend.responses;
 
 import java.util.UUID;
 
+/**
+ * Model, representing response sent, when deleting request was received.
+ */
 public class DeletedResponse {
   public UUID deletedId;
 
