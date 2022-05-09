d6de85843c87c42f0c92823d86afcf660272279b
Daniil <dadyarri@gmail.com>
2022-05-09 19:26:33 +0300

    feat(util): add method to merge two objects

--- src/main/java/ru/zgz/star/backend/util/ClassUtil.java
+++ src/main/java/ru/zgz/star/backend/util/ClassUtil.java
@@ -2,6 +2,9 @@ package ru.zgz.star.backend.util;
 
 import com.google.common.reflect.ClassPath;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,5 +22,43 @@ public class ClassUtil {
         .filter(clazz -> clazz.getPackageName().equalsIgnoreCase(packageName))
         .map(ClassPath.ClassInfo::load)
         .collect(Collectors.toList());
+  }
+
+  /**
+   * Merges two objects into new one.
+   * Iterates through all fields of the first object and, if null takes value from the second
+   *
+   * @param one first object
+   * @param another second object
+   * @return new object
+   */
+  public static Object mergeObjects(Object one, Object another) {
+    for (Field field : one.getClass().getDeclaredFields()) {
+      Object value = getFieldValue(one, field);
+      if (value == null) {
+        try {
+          field.setAccessible(true);
+          field.set(one, getFieldValue(another, field));
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return one;
+  }
+
+  private static Object getFieldValue(Object instance, Field field) {
+    try {
+      for (Method method : instance.getClass().getDeclaredMethods()) {
+        if (method.getName().equalsIgnoreCase("get" + field.getName())
+            && method.getName().length() == field.getName().length() + 3) {
+          return method.invoke(instance);
+        }
+      }
+    } catch (InvocationTargetException | IllegalAccessException e) {
+      e.printStackTrace();
+      return null;
+    }
+    return null;
   }
 }
