c0d9f99205087de74ad6e3475f41933ecd5a3832
Daniil <dadyarri@gmail.com>
2022-05-09 09:29:53 +0300

    style(test): fix indent

--- src/test/groovy/daos/AccountDAOSpecification.groovy
+++ src/test/groovy/daos/AccountDAOSpecification.groovy
@@ -10,7 +10,7 @@ class AccountDAOSpecification extends Specification {
 
   @Shared
           emailAddress = new Faker().internet().emailAddress()
-  
+
   def "Create new account in database and verify it"() {
     given: "Creating data access object"
       AccountDao dao = new AccountDao()
