3d364fee8f489f90b33c8d81b2165a426432756f
Daniil <dadyarri@gmail.com>
2022-05-10 11:46:50 +0300

    feat(docs): add javadocs

--- src/main/java/ru/zgz/star/backend/exceptions/http/BadRequestException.java
+++ src/main/java/ru/zgz/star/backend/exceptions/http/BadRequestException.java
@@ -1,7 +1,8 @@
 package ru.zgz.star.backend.exceptions.http;
 
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-        super(message);
-    }
+/** Exception for bad request. */
+public class BadRequestException extends RuntimeException {
+  public BadRequestException(String message) {
+    super(message);
+  }
 }
--- src/main/java/ru/zgz/star/backend/exceptions/http/MethodNotAllowedException.java
+++ src/main/java/ru/zgz/star/backend/exceptions/http/MethodNotAllowedException.java
@@ -1,5 +1,6 @@
 package ru.zgz.star.backend.exceptions.http;
 
+/** Exception thrown when the HTTP method is not allowed. */
 public class MethodNotAllowedException extends RuntimeException {
   public MethodNotAllowedException(String message) {
     super(message);
