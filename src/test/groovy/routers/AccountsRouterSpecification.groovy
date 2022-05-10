5859c33255e9f87598e1c970ce89e1931fcd1149
Daniil <dadyarri@gmail.com>
2022-05-10 10:33:59 +0300

    feat(test): add some tests for unsupported http methods

--- src/test/groovy/routers/AccountsRouterSpecification.groovy
+++ src/test/groovy/routers/AccountsRouterSpecification.groovy
@@ -6,6 +6,7 @@ import com.google.gson.JsonParseException
 import com.mashape.unirest.http.Unirest
 import ru.zgz.star.backend.daos.AccountDao
 import ru.zgz.star.backend.models.Account
+import ru.zgz.star.backend.responses.ErrorResponse
 import spock.lang.Shared
 import util.BaseRouterSpecification
 
@@ -57,6 +58,42 @@ class AccountsRouterSpecification extends BaseRouterSpecification {
       response.getStatus() == 201
       account != null
       account.getEmail() == sampleAccount.getEmail()
+  }
+
+  def "PUT request should return MethodNotAllowed"() {
+    given:
+      def response = Unirest.put("${BASE_URL}").asString()
+
+    when:
+      ErrorResponse error = new Gson().fromJson(response.body, ErrorResponse)
+
+    then:
+      response.getStatus() == 405
+      error.message == "Method PUT is not allowed for this resource"
+  }
+
+  def "PATCH request should return MethodNotAllowed"() {
+    given:
+      def response = Unirest.patch("${BASE_URL}").asString()
+
+    when:
+      ErrorResponse error = new Gson().fromJson(response.body, ErrorResponse)
+
+    then:
+      response.getStatus() == 405
+      error.message == "Method PATCH is not allowed for this resource"
+  }
+
+  def "DELETE request should return MethodNotAllowed"() {
+    given:
+      def response = Unirest.delete("${BASE_URL}").asString()
+
+    when:
+      ErrorResponse error = new Gson().fromJson(response.body, ErrorResponse)
+
+    then:
+      response.getStatus() == 405
+      error.message == "Method DELETE is not allowed for this resource"
   }
 
   def cleanup() {
