ffccc355f1a0345a9562d723d90244e333b5c17f
Daniil <dadyarri@gmail.com>
2022-05-12 13:38:49 +0300

    feat(daos): add invite code's dao

---
+++ src/main/java/ru/zgz/star/backend/daos/InviteCodeDao.java
@@ -0,0 +1,198 @@
+package ru.zgz.star.backend.daos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import ru.zgz.star.backend.models.InviteCode;
+import ru.zgz.star.backend.util.DbUtil;
+
+/** DAO for invite_code table. */
+public class InviteCodeDao {
+  private final Connection connection;
+
+  /** Instantiates a new InviteCode dao. */
+  public InviteCodeDao() {
+    this.connection = DbUtil.getConnection();
+  }
+
+  /**
+   * Instantiates a new InviteCode dao.
+   *
+   * @param connection the connection
+   */
+  public InviteCodeDao(Connection connection) {
+    this.connection = connection;
+  }
+
+  /**
+   * Checks if invite code exists.
+   *
+   * @param id id of account
+   * @return true if account exists
+   */
+  public Boolean findById(java.util.UUID id) {
+    try {
+      PreparedStatement query =
+          connection.prepareStatement("select count(*) from invite_code where id=?");
+      query.setObject(1, id);
+      ResultSet rs = query.executeQuery();
+      if (rs.next()) {
+        return rs.getInt(1) > 0;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return false;
+  }
+
+  /**
+   * Updates account.
+   *
+   * @param inviteCode updated invite code
+   */
+  public void update(InviteCode inviteCode) {
+    try {
+      PreparedStatement query =
+          connection.prepareStatement(
+              "update invite_code set invite_code=?, activated_date=?, account_id=?, is_valid=? where id=?");
+      query.setObject(1, inviteCode.getInviteCode());
+      query.setObject(2, inviteCode.getActivationDate());
+      query.setObject(3, inviteCode.getAccount());
+      query.setObject(4, inviteCode.getIsValid());
+      query.setObject(5, inviteCode.getId());
+      query.executeUpdate();
+      query.close();
+      connection.commit();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+  /**
+   * Create new invite code.
+   *
+   * @param inviteCode the invite code
+   */
+  public void add(InviteCode inviteCode) {
+    try {
+      PreparedStatement query =
+          connection.prepareStatement(
+              "insert into invite_code(account_id, invite_code, activated_date, is_valid) values (?, ?, ?, ?);");
+      query.setObject(1, inviteCode.getAccount());
+      query.setString(2, inviteCode.getInviteCode());
+      query.setInt(3, inviteCode.getActivationDate());
+      query.setBoolean(4, inviteCode.getIsValid());
+      query.executeUpdate();
+      query.close();
+      connection.commit();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Gets all invite codes.
+   *
+   * @return list of invite codes
+   */
+  public List<InviteCode> getAll() {
+    List<InviteCode> inviteCodes = new ArrayList<>();
+    try {
+      Statement st = connection.createStatement();
+      ResultSet rs = st.executeQuery("select * from invite_code");
+      while (rs.next()) {
+        inviteCodes.add(
+            new InviteCode()
+                .setId(UUID.fromString(rs.getString("id")))
+                .setInviteCode(rs.getString("invite_code"))
+                .setActivationDate(rs.getInt("activated_date"))
+                .setIsValid(rs.getBoolean("is_valid")));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return inviteCodes;
+  }
+
+  /**
+   * Gets exact invite code by id.
+   *
+   * @param id id of invite code
+   * @return exact invite code
+   */
+  public InviteCode getById(String id) {
+    try {
+      PreparedStatement query = connection.prepareStatement("select * from invite_code where id=?");
+      query.setObject(1, UUID.fromString(id));
+      ResultSet rs = query.executeQuery();
+      return buildInviteCode(rs);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  /**
+   * Gets exact invite code by code.
+   *
+   * @param code invite code
+   * @return exact invite code
+   */
+  public InviteCode getByCode(String code) {
+    try {
+      PreparedStatement query =
+          connection.prepareStatement("select * from invite_code where invite_code=?");
+      query.setString(1, code);
+      ResultSet rs = query.executeQuery();
+      return buildInviteCode(rs);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  /**
+   * Delete exact invite code by id.
+   *
+   * @param id id of invite code
+   */
+  public void deleteById(java.util.UUID id) {
+    try {
+      PreparedStatement st = connection.prepareStatement("delete from invite_code where id=?");
+      st.setObject(1, id);
+      connection.commit();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /** Delete all accounts. */
+  @SuppressWarnings("SqlWithoutWhere")
+  public void deleteAll() {
+    try {
+      Statement st = connection.createStatement();
+      st.executeUpdate("delete from invite_code");
+      connection.commit();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private InviteCode buildInviteCode(ResultSet rs) throws SQLException {
+    if (rs.next()) {
+      return new InviteCode()
+          .setId(UUID.fromString(rs.getString("id")))
+          .setInviteCode(rs.getString("invite_code"))
+          .setIsValid(rs.getBoolean("is_valid"))
+          .setAccount(UUID.fromString(rs.getString("account_id")))
+          .setActivationDate(rs.getInt("activated_date"));
+    } else {
+      return null;
+    }
+  }
+}
