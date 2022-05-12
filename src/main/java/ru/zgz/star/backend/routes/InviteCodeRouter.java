35b38b2a1e007f5a6ad1daba1a990df8a7b93fb6
Daniil <dadyarri@gmail.com>
2022-05-13 02:31:09 +0300

    feat(routers): add 405's methods for /inviteCodes router

--- src/main/java/ru/zgz/star/backend/routes/InviteCodeRouter.java
+++ src/main/java/ru/zgz/star/backend/routes/InviteCodeRouter.java
@@ -2,6 +2,7 @@ package ru.zgz.star.backend.routes;
 
 import com.google.gson.Gson;
 import ru.zgz.star.backend.daos.InviteCodeDao;
+import ru.zgz.star.backend.exceptions.http.MethodNotAllowedException;
 import ru.zgz.star.backend.models.InviteCode;
 import spark.Request;
 import spark.Response;
@@ -11,6 +12,19 @@ public class InviteCodeRouter {
 
   /** Base path for all requests, which this router handles. */
   public static final String BASE_URL = "/inviteCodes";
+
+  /**
+   * Handles GET requests to /inviteCodes.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of the created invite code
+   */
+  public static String get(Request request, Response response) {
+    response.type("application/json");
+    response.status(405);
+    throw new MethodNotAllowedException("Method GET is not allowed for this resource");
+  }
 
   /**
    * Handles POST requests to /inviteCodes.
@@ -26,5 +40,44 @@ public class InviteCodeRouter {
     InviteCode inviteCode = new Gson().fromJson(request.body(), InviteCode.class);
     inviteCodeDao.add(inviteCode);
     return new Gson().toJson(inviteCode);
+  }
+
+  /**
+   * Handles PUT requests to /inviteCodes.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of the created invite code
+   */
+  public static String put(Request request, Response response) {
+    response.type("application/json");
+    response.status(405);
+    throw new MethodNotAllowedException("Method PUT is not allowed for this resource");
+  }
+
+  /**
+   * Handles PATCH requests to /inviteCodes.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of the created invite code
+   */
+  public static String patch(Request request, Response response) {
+    response.type("application/json");
+    response.status(405);
+    throw new MethodNotAllowedException("Method PATCH is not allowed for this resource");
+  }
+
+  /**
+   * Handles DELETE requests to /inviteCodes.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of the created invite code
+   */
+  public static String delete(Request request, Response response) {
+    response.type("application/json");
+    response.status(405);
+    throw new MethodNotAllowedException("Method PATCH is not allowed for this resource");
   }
 }
