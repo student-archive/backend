899f971c356a0037d55381d9e211097fa2ee3664
Daniil <dadyarri@gmail.com>
2022-05-16 15:01:27 +0300

    feat(docs): add some javadocs

--- src/main/java/ru/zgz/star/backend/exceptions/http/BaseHttpException.java
+++ src/main/java/ru/zgz/star/backend/exceptions/http/BaseHttpException.java
@@ -1,5 +1,8 @@
 package ru.zgz.star.backend.exceptions.http;
 
+/**
+ * Base HTTP exception for all HTTP exceptions.
+ */
 public class BaseHttpException extends RuntimeException {
 
   public BaseHttpException(String message) {
