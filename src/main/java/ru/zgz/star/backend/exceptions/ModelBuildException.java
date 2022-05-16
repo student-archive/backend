a7ddedc9ccf0846cdef0710ce9fe5a8adcf9bec9
Daniil <dadyarri@gmail.com>
2022-05-16 15:02:56 +0300

    style(docs): reformat javadocs

--- src/main/java/ru/zgz/star/backend/exceptions/ModelBuildException.java
+++ src/main/java/ru/zgz/star/backend/exceptions/ModelBuildException.java
@@ -1,8 +1,6 @@
 package ru.zgz.star.backend.exceptions;
 
-/**
- * Exception, raised when model building failed.
- */
+/** Exception, raised when model building failed. */
 public class ModelBuildException extends RuntimeException {
   public ModelBuildException(String message) {
     super(message);
--- src/main/java/ru/zgz/star/backend/exceptions/http/BaseHttpException.java
+++ src/main/java/ru/zgz/star/backend/exceptions/http/BaseHttpException.java
@@ -1,8 +1,6 @@
 package ru.zgz.star.backend.exceptions.http;
 
-/**
- * Base HTTP exception for all HTTP exceptions.
- */
+/** Base HTTP exception for all HTTP exceptions. */
 public class BaseHttpException extends RuntimeException {
 
   public BaseHttpException(String message) {
