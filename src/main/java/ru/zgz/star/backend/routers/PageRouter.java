6121b69c14ca256098bcf895cbdf670058c07ac4
ironalex <superzhelezov@gmail.com>
2022-05-17 23:49:38 +0300

    feat(routers): add PageRouter

---
+++ src/main/java/ru/zgz/star/backend/routers/PageRouter.java
@@ -0,0 +1,135 @@
+package ru.zgz.star.backend.routers;
+
+import com.google.gson.Gson;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.zgz.star.backend.daos.EmployeePositionDao;
+import ru.zgz.star.backend.daos.PageDao;
+import ru.zgz.star.backend.exceptions.http.BadRequestException;
+import ru.zgz.star.backend.exceptions.http.MethodNotAllowedException;
+import ru.zgz.star.backend.exceptions.http.ResourceNotFoundException;
+import ru.zgz.star.backend.models.EmployeePosition;
+import ru.zgz.star.backend.models.Page;
+import ru.zgz.star.backend.responses.DeletedResponse;
+import ru.zgz.star.backend.util.ClassUtil;
+import spark.Request;
+import spark.Response;
+
+/** Router, which handles requests to /page/:id. */
+public class PageRouter {
+
+  /** Base path for all requests, which this router handles. */
+  public static String BASE_URL = "/page/:id";
+  /** Logger for this class. */
+  public static Logger logger = LoggerFactory.getLogger(PageRouter.class);
+
+  /**
+   * Handles GET request to /page/:id.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of exact page
+   */
+  public static String get(Request request, Response response) {
+    response.type("application/json");
+    PageDao dao = new PageDao();
+    try {
+      if (dao.findById(UUID.fromString(request.params("id")))) {
+        Page page = dao.getById(request.params("id"));
+        return new Gson().toJson(page);
+      } else {
+        response.status(404);
+        throw new ResourceNotFoundException(
+            String.format("Page id=%s not found", request.params("id")));
+      }
+    } catch (IllegalArgumentException e) {
+      response.status(400);
+      throw new BadRequestException(
+          String.format("Given UUID (%s) is not valid", request.params("id")));
+    }
+  }
+
+  /**
+   * Handles DELETE request to /page/:id.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of id of deleted page
+   */
+  public static String delete(Request request, Response response) {
+    response.type("application/json");
+    PageDao dao = new PageDao();
+    try {
+      if (dao.findById(UUID.fromString(request.params("id")))) {
+        dao.deleteById(UUID.fromString(request.params("id")));
+        logger.info("Page {} deleted", request.params("id"));
+        return new Gson().toJson(new DeletedResponse(UUID.fromString(request.params("id"))));
+      } else {
+        response.status(404);
+        throw new ResourceNotFoundException(
+            String.format("Page id=%s not found", request.params("id")));
+      }
+    } catch (IllegalArgumentException e) {
+      response.status(400);
+      throw new BadRequestException("Passed UUID is not valid");
+    }
+  }
+
+  /**
+   * Handles PATCH request to /page/:id.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of updated page
+   */
+  public static String patch(Request request, Response response) {
+    response.type("application/json");
+    PageDao dao = new PageDao();
+
+    try {
+      if (dao.findById(UUID.fromString(request.params("id")))) {
+        logger.info("Page {} found", request.params("id"));
+        Page page = dao.getById(request.params("id"));
+        Page body = new Gson().fromJson(request.body(), Page.class);
+        body.setId(page.getId());
+        Page updated = (Page) ClassUtil.mergeObjects(body, page);
+        dao.update(updated);
+        return new Gson().toJson(updated);
+      } else {
+        response.status(404);
+        throw new ResourceNotFoundException(
+            String.format("Page id=%s not found", request.params("id")));
+      }
+    } catch (IllegalArgumentException e) {
+      response.status(400);
+      throw new BadRequestException("Passed UUID is not valid");
+    }
+  }
+
+  /**
+   * Handles PUT request to /page/:id.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of 405 error
+   */
+  public static String put(Request request, Response response) {
+    response.type("application/json");
+    response.status(405);
+    throw new MethodNotAllowedException("Method PUT is not allowed for this resource");
+  }
+
+  /**
+   * Handles POST request to /page/:id.
+   *
+   * @param request request object
+   * @param response response object
+   * @return JSON representation of 405 error
+   */
+  public static String post(Request request, Response response) {
+    response.type("application/json");
+    response.status(405);
+    throw new MethodNotAllowedException("Method POST is not allowed for this resource");
+  }
+}
